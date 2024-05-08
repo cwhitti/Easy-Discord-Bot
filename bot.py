df638969a1ce083b73d4d38a8d2712f5c9bfb3a0
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-05-07 23:49:05 -0700

    Created secret, gitignore, and updated bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,39 @@
 import discord
 from secret import TOKEN
 
-#from classes.course import Course
-
 def run_discord_bot():
 
-    # set up bot
+    # Set up bot
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client( intents=intents )
 
-    user_cooldowns = {}
+    # Set prefix
+    prefix = '!'
 
-    #Show bot logged on successfully
+    # Show bot logged on successfully
     @client.event
     async def on_ready():
 
         await client.change_presence(activity=discord.Game(name="New Bot!"))
         print(f'{client.user} is now running!')
 
-    # handle message
-    #@client.event
-    #async def on_message(msg):
+    # Message Handler
+    @client.event
+    async def on_message(msg):
 
+        # Grab the Message Content
+        content = msg.content
 
+        # Check if the message starts with a known command
+        if content.startswith(prefix + "hello"):
+
+            # Grab the author's name
+            author = msg.author.name
+
+            # Send hello back
+            await msg.channel.send(f"Hello, {author}!")
+
+    # Run Bot with Token
+        # Should be  the very last command inside of run_discord_bot 
     client.run( TOKEN )
